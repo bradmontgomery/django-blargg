c2cce6254529e45c88baa2d4fa8f04d5bba53eb6
Brad Montgomery <brad@bradmontgomery.net>
2013-01-06 17:34:31 -0600

    added a setup, manifest, and set a version

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from blargg import __version__
+
+setup(
+    name='django-blargg',
+    version=__version__,
+    description="yet another blog app; this one aims to be fairly minimal",
+    long_description=open('README.rst').read(),
+    author='Brad Montgomery',
+    author_email='brad@bradmontgomery.net',
+    url='https://github.com/bradmontgomery/django-blargg',
+    license='MIT',
+    packages=['blargg'],
+    include_package_data=True,
+    package_data={
+        '': ['README.rst'],
+        'blargg': ['templates/blargg/*.html']
+    },
+    zip_safe=False,
+    install_requires=['django>=1.5'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
