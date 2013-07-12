9a03acc2f60a05d8dc71bc09b0fcb81470e6e7ab
Brad Montgomery <brad@bradmontgomery.net>
2013-07-12 14:47:09 -0500

    added tests for feeds

--- blargg/tests/__init__.py
+++ blargg/tests/__init__.py
@@ -1,12 +1,15 @@
 from .admin import TestEntryAdmin, TestTagAdmin
+from .feeds import TestRSSEntriesFeed, TestAtomEntriesFeed
 from .models import TestEntry, TestTag, TestTagManager
 from .sitemaps import TestEntrySitemap
 from .views import TestViews
 
 __all__ = (
+    TestAtomEntriesFeed,
     TestEntry,
     TestEntryAdmin,
     TestEntrySitemap,
+    TestRSSEntriesFeed,
     TestTag,
     TestTagAdmin,
     TestTagManager,
---
+++ blargg/tests/feeds.py
@@ -0,0 +1,101 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.test import TestCase
+
+from ..feeds import RSSEntriesFeed, AtomEntriesFeed
+from ..models import Entry
+
+
+class TestRSSEntriesFeed(TestCase):
+
+    def setUp(self):
+        # Some setup (need a User and an Entry)
+        username = 'blargg'
+        user = User.objects.create(
+            username=username,
+            password='{0}@example.com'.format(username)
+        )
+        self.entry = Entry(
+            site=Site.objects.get(pk=settings.SITE_ID),
+            author=user,
+            title="Test Entry",
+            raw_content="Test Content",
+            content_format="html"
+        )
+        self.entry.publish()  # calls .save()
+
+        self.unpublished_entry = Entry(
+            site=Site.objects.get(pk=settings.SITE_ID),
+            author=user,
+            title="Test Entry",
+            raw_content="Test Content",
+            content_format="html",
+            published=False
+        )
+        self.entry.save()
+
+        self.feed = RSSEntriesFeed()
+
+    def test_items(self):
+        """Verify that ``.items()`` returns a queryset of published Entries"""
+        published_entries = list(Entry.objects.filter(published=True))
+        feed_entries = list(self.feed.items())
+        self.assertEqual(feed_entries, published_entries)
+        self.assertNotIn(self.unpublished_entry, feed_entries)
+
+    def test_item_title(self):
+        self.assertEqual(self.feed.item_title(self.entry), self.entry.title)
+
+    def test_item_description(self):
+        self.assertEqual(
+            self.feed.item_description(self.entry),
+            self.entry.content
+        )
+
+
+class TestAtomEntriesFeed(TestCase):
+    """NOTE: This whole test is a duplicate of ``TestRSSEntriesFeed``."""
+    def setUp(self):
+        # Some setup (need a User and an Entry)
+        username = 'blargg'
+        user = User.objects.create(
+            username=username,
+            password='{0}@example.com'.format(username)
+        )
+        self.entry = Entry(
+            site=Site.objects.get(pk=settings.SITE_ID),
+            author=user,
+            title="Test Entry",
+            raw_content="Test Content",
+            content_format="html"
+        )
+        self.entry.publish()  # calls .save()
+
+        self.unpublished_entry = Entry(
+            site=Site.objects.get(pk=settings.SITE_ID),
+            author=user,
+            title="Test Entry",
+            raw_content="Test Content",
+            content_format="html",
+            published=False
+        )
+        self.entry.save()
+
+        self.feed = AtomEntriesFeed()
+
+    def test_items(self):
+        """Verify that ``.items()`` returns a queryset of published Entries"""
+        published_entries = list(Entry.objects.filter(published=True))
+        feed_entries = list(self.feed.items())
+        self.assertEqual(feed_entries, published_entries)
+        self.assertNotIn(self.unpublished_entry, feed_entries)
+
+    def test_item_title(self):
+        self.assertEqual(self.feed.item_title(self.entry), self.entry.title)
+
+    def test_item_description(self):
+        self.assertEqual(
+            self.feed.item_description(self.entry),
+            self.entry.content
+        )
