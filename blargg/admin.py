19da22b9cb96448f91eb8c74cc2f8bd71b905742
Brad Montgomery <brad@bradmontgomery.net>
2015-05-03 16:41:38 -0500

    list and filter by content format in the admin

--- blargg/admin.py
+++ blargg/admin.py
@@ -9,9 +9,11 @@ class TagAdmin(admin.ModelAdmin):
 
 class EntryAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'author', 'published', 'published_on', 'updated_on')
+        'title', 'content_format', 'author', 'published',
+        'published_on', 'updated_on'
+    )
     date_hierarchy = 'created_on'
-    list_filter = ('published', )
+    list_filter = ('published', 'content_format')
     search_fields = ('title', 'raw_content', 'tag_string')
     prepopulated_fields = {"slug": ("title", )}
     actions = ['publish_entries']
