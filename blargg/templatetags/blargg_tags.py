cf0293f2b6ddefaa42299ecc560a2fda6660b7ba
Brad Montgomery <brad@bradmontgomery.net>
2016-01-07 18:08:59 -0600

    updating url namespace in default templates and templatetags

--- blargg/templatetags/blargg_tags.py
+++ blargg/templatetags/blargg_tags.py
@@ -10,4 +10,4 @@ def entry_archive_year_url():
     """Renders the ``entry_archive_year`` URL for the latest ``Entry``."""
     entry = Entry.objects.filter(published=True).latest()
     arg_list = [entry.published_on.strftime("%Y")]
-    return reverse('entry_archive_year', args=arg_list)
+    return reverse('blargg:entry_archive_year', args=arg_list)
